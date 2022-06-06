403f51b84023be818f040991b9ebb80313428143
ton-tonton <phantom.ness@windowslive.com>
2022-06-06 11:26:03 +0700

    Initial project

---
+++ lib/switchs/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Switchs
+  VERSION = "0.1.0"
+end
---
+++ spec/switchs_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Switchs do
+  it "has a version number" do
+    expect(Switchs::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
